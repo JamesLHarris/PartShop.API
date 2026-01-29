afc2bd3d09ea7faf35f066cf165e82cd3cfefbe4
JamesLHarris <James.Larson.Harris@gmail.com>
2026-01-28 20:06:36 -0800

    API calls and service files for Customer Search Functionality

---
+++ Site_2024/Models/PartCustomer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Site_2024.Models.Domain.Parts
+{
+    public class PartCustomer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int CatagoryId { get; set; }
+        public string CatagoryName { get; set; }
+
+        public int MakeId { get; set; }
+        public string MakeName { get; set; }
+
+        public int ModelId { get; set; }
+        public string ModelName { get; set; }
+
+        public int Year { get; set; }
+        public string PartNumber { get; set; }
+
+        public bool Rusted { get; set; }
+        public bool Tested { get; set; }
+
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+        public int AvailableId { get; set; }
+        public string AvailableStatus { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
+
---
+++ Site_2024/Requests/CustomerSearchRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Site_2024.Web.Api.Requests
+{
+    public class CustomerSearchRequest
+    {
+        public string? q { get; set; }
+        public int? CatagoryId { get; set; }
+        public int? MakeId { get; set; }
+        public int? ModelId { get; set; }
+        public int? YearMin { get; set; }
+        public int? YearMax { get; set; }
+        public int? AvailableId { get; set; }
+        public decimal? PriceMin { get; set; }
+        public decimal? PriceMax { get; set; }
+        public bool? Rusted { get; set; }
+        public bool? Tested { get; set; }
+    }
+}
